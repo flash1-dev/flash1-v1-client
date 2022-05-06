688757cf7dd3991cf67a0ae546cbcfd9a7351b98
ycryptx <yonatan.med@gmail.com>
2022-05-06 12:04:27 -0400

    rename to tests

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,12 @@
 module.exports = {
   /* eslint-disable global-require */
-  preset: 'ts-jest',
-  roots: [
-    '<rootDir>/__tests__',
-  ],
-  testRegex: '__tests__\\/.*\\.test\\.ts$',
-  moduleFileExtensions: [
-    'js',
-    'ts',
-    'json',
-    'node',
-  ],
+  preset: "ts-jest",
+  roots: ["<rootDir>/tests"],
+  testRegex: "tests\\/.*\\.test\\.ts$",
+  moduleFileExtensions: ["js", "ts", "json", "node"],
   resetMocks: true,
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  testEnvironment: 'node',
+  setupFilesAfterEnv: ["./jest.setup.js"],
+  testEnvironment: "node",
   testTimeout: 30000,
-  name: 'v1-client',
+  name: "v1-client",
 };
